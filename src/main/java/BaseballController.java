e57e36bc6459b9d7d752715eaadfddbb06481cd6
KimJS0328 <wjddkel328@gmail.com>
2024-03-22 15:58:10 +0900

    feat(Model): 모델 로직 구현 및 테스트 작성

--- src/main/java/BaseballController.java
+++ src/main/java/BaseballController.java
@@ -7,44 +7,44 @@ public class BaseballController {
 		this.view = view;
 	}
 
-	public void run() {
-		model.init();
-		view.printIntro();
-		while (true) {
-			runOneGame();
-
-			view.printControlGuide();
-			int control = view.scanControl();
-			while (!model.validateControl(control)) {
-				view.printControlError();
-				view.printControlGuide();
-				control = view.scanControl();
-			}
-
-			if (model.isQuit(control)) {
-				return;
-			}
-		}
-	}
-
-	private void runOneGame() {
-		while (true) {
-			BaseballResult result = runOneTurn();
-			if (model.isGameWin(model)) {
-				view.printOutro();
-				return;
-			}
-			view.printResult(result);
-		}
-	}
-
-	private BaseballResult runOneTurn() {
-		view.printInputGuide();
-		int number = view.scanBaseballNumber();
-		if (!model.validateNumber(number)) {
-			view.printInputError();
-			return null;
-		}
-		return model.compareNumber(number);
-	}
+	// public void run() {
+	// 	model.init();
+	// 	view.printIntro();
+	// 	while (true) {
+	// 		runOneGame();
+	//
+	// 		view.printControlGuide();
+	// 		int control = view.scanControl();
+	// 		while (!model.isValidControl(control)) {
+	// 			view.printControlError();
+	// 			view.printControlGuide();
+	// 			control = view.scanControl();
+	// 		}
+	//
+	// 		if (model.isQuit(control)) {
+	// 			return;
+	// 		}
+	// 	}
+	// }
+	//
+	// private void runOneGame() {
+	// 	while (true) {
+	// 		BaseballResult result = runOneTurn();
+	// 		if (result.isThreeStrike()) {
+	// 			view.printOutro();
+	// 			return;
+	// 		}
+	// 		view.printResult(result);
+	// 	}
+	// }
+	//
+	// private BaseballResult runOneTurn() {
+	// 	view.printInputGuide();
+	// 	int number = view.scanBaseballNumber();
+	// 	if (!model.isValidNumber(number)) {
+	// 		view.printInputError();
+	// 		return null;
+	// 	}
+	// 	return model.compareNumber(number);
+	// }
 }
--- src/main/java/BaseballResult.java
+++ src/main/java/BaseballResult.java
@@ -1,4 +1,29 @@
 public class BaseballResult {
 	private int strike;
 	private int ball;
+
+	public BaseballResult() {
+		this.strike = 0;
+		this.ball = 0;
+	}
+
+	public void addOneStrike() {
+		++strike;
+	}
+
+	public void addOneBall() {
+		++ball;
+	}
+
+	public boolean isThreeStrike() {
+		return strike == 3;
+	}
+
+	public int getBall() {
+		return ball;
+	}
+
+	public int getStrike() {
+		return strike;
+	}
 }
